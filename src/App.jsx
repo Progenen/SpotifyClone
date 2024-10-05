af6bca8959d30002abbc271d5ef9c9017ecbfdb8
Stanislav-Mac <stanislav_2010@icloud.com>
2024-10-05 18:18:40 +0500

    Start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='h-screen bg-black'>
+          <div className='h-[90%] flex'>
+            <Sidebar/>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
